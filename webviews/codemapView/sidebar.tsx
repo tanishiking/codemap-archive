22ce6400ca857322dce555a6010a54122b0d1ac8
Rikito Taniguchi <rikiriki1238@gmail.com>
2021-11-16 01:44:10 +0900

    Minimum styling

--- webviews/codemapView/sidebar.tsx
+++ webviews/codemapView/sidebar.tsx
@@ -1,5 +1,18 @@
 import * as React from "react";
+import styled from "styled-components";
 import { TemporalNode } from "./temporalNode";
+
+const NodeBlock = styled.div`
+  align-items: center;
+  border: 1px solid #1a192b;
+  color: #1a192b;
+  background-color: #fff;
+  border-radius: 2px;
+  display: flex;
+  height: 20px;
+  justify-content: center";
+  padding: 4px;
+`;
 
 export const SidebarComponent = (props: { nodes: TemporalNode[] }) => {
   const onDragStart = (event: React.DragEvent, node: TemporalNode) => {
@@ -14,14 +27,13 @@ export const SidebarComponent = (props: { nodes: TemporalNode[] }) => {
         You can drag these nodes to the pane on the right.
       </div>
       {props.nodes.map((node) => (
-        <div
-          className="react-flow__node"
+        <NodeBlock
           key={node.id}
           onDragStart={(event) => onDragStart(event, node)}
           draggable
         >
           {node.label}
-        </div>
+        </NodeBlock>
       ))}
     </aside>
   );
